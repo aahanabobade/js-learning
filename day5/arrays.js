15705ecd4a8dff9d715aaf072bb90498e04f6368
Aahana Bobade <aahanabobade@gmail.com>
2026-01-08 17:07:20 +0530

    arrays in javascript

--- day5/arrays.js
+++ day5/arrays.js
@@ -1,6 +1,27 @@
 var countries = ["India","Japan"]
 
-var countries1 = new Array ("india","rajasthan")
+var countries1 = new Array ("india","rajasthan","Assam")
 
 console.log(countries1[0])
-console.log(countries.length)
+
+console.log(countries.length) //gives length
+
+countries1[0] = "Punjab"
+console.log(countries1)  //replace 1st element of countries 1
+
+var user = ["aahana" ,"aahana@gmail.com", 1,22,true];
+console.log(user)
+
+user.pop();
+user.pop();
+console.log(user)
+
+user.unshift("aahanabobade")
+console.log(user)
+
+user.shift();
+console.log(user)
+
+console.log(user.indexOf("aahana"))
+
+console.log(Array.from("aahana"));
