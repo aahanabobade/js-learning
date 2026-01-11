42b98b368ddd2cfe716d4571e2a987be31091f7e
Aahana Bobade <aahanabobade@gmail.com>
2026-01-11 11:06:11 +0530

    day 8: while and do while

---
+++ day8/whileanddowhile.js
@@ -0,0 +1,15 @@
+const myStates = ["Rajasthan","Sikkim","assam",1947,"punjab","haryana","gujrat","goa"];
+
+//while
+let i = 0;
+while(i<myStates.length){
+    console.log(myStates[i])
+    i++
+}
+
+//do while
+let j = 10
+do{
+    console.log(j);
+j++
+}while(j<3)
