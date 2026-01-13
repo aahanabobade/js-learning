96c42c996ef3c7c21ff7553f07ebaff4094faeef
aahanabobade <your_email@example.com>
2026-01-13 17:13:30 +0530

    day 10:event listener

---
+++ day10/eventListener.js
@@ -0,0 +1,7 @@
+//documentation:https://developer.mozilla.org/en-US/docs/Web/API/Event
+
+const orangeElement = getBGcolour(pink);
+
+pink.addEventListener('mouseenter',()=>{
+    center.style.background =orangeElement
+})
