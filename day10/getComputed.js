24ca870f5077cc2a81d0e09c7cb36e7b7b8f1faf
aahanabobade <your_email@example.com>
2026-01-13 17:05:23 +0530

    day 10:get computed element

---
+++ day10/getComputed.js
@@ -0,0 +1,6 @@
+console.log(window.getComputedStyle(red).backgroundColor) //gives output in rbg
+
+const getBGcolour=(selectedElement)=>{
+    return window.getComputedStyle(red).backgroundColor
+};
+console.log(getBGcolour(pink))
