f8af017889898dea06d9e556843f25629f96e205
Aahana Bobade <aahanabobade@gmail.com>
2026-01-18 12:42:53 +0530

    day 13: lexical scoping

---
+++ day13/lexicalscoping.js
@@ -0,0 +1,11 @@
+function init(){
+    var firstName = "aahana"
+    function sayFirstname(){
+        console.log(firstName)
+    }
+    sayFirstname()
+}
+
+init()
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Closures
