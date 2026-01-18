bb0233f92bb5392438713af1c1c5860bbc24206a
Aahana Bobade <aahanabobade@gmail.com>
2026-01-18 12:13:29 +0530

    day 13: object chain in js

---
+++ day13/objectchain.js
@@ -0,0 +1,24 @@
+var User = function(firstName,courseCount){
+    this.firstName= firstName;
+    this.courseCount=courseCount;
+    this.getCoursecount = function(){
+        console.log(`Course count is:${this.courseCount}`)
+    }
+}
+
+//proto
+
+User.prototype.getFirstname = function(){
+    console.log(`your name is : ${this.firstName}`)
+}
+
+var aahana = new User("aahana",2)
+aahana.getCoursecount()
+aahana.getFirstname()
+
+if (aahana.hasOwnProperty("firstName")){
+    aahana.getFirstname()
+}
+
+var chinu = new User("chinu",8)
+chinu.getCoursecount()
