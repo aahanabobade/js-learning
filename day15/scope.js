c112dc4f8815e69bc4f07381466e9c231919e35a
Aahana Bobade <aahanabobade@gmail.com>
2026-01-25 12:24:59 +0530

    day 15 : exploring scope in js

---
+++ day15/scope.js
@@ -0,0 +1,18 @@
+//let, const,var
+
+//scope is a big issue in js
+//{} => scope in other language
+
+//fn : scope in js
+
+//console.log(name);
+
+
+var name = "aahana"
+
+if (true){
+    let lastname = "bobade"
+    
+    
+}
+console.log(lastname);
