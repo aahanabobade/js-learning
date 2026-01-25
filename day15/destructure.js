634cf31acfda1320468f375b0cf979fa33ee6606
Aahana Bobade <aahanabobade@gmail.com>
2026-01-25 13:01:02 +0530

    day 15 : destructure data in js

---
+++ day15/destructure.js
@@ -0,0 +1,17 @@
+const name = ["aahana","chinu","priyanka","tom"]
+// var role = name[2]
+// var user = name[0]
+var [child, sister, mom, brother] = name
+console.log(child);
+
+
+const myUser = {
+    namee :"aahana",
+    coursecount :2,
+    role:"admin"
+}
+
+console.log(myUser.role)
+
+const {namee,coursecount,role} = myUser
+console.log(coursecount);
