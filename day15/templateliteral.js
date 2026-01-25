0948f40fce011e47cad4d37cb4ff8b5d17ed5be3
Aahana Bobade <aahanabobade@gmail.com>
2026-01-25 12:35:11 +0530

    day 15 : template literals in js

---
+++ day15/templateliteral.js
@@ -0,0 +1,12 @@
+var name = "aahana";
+var fullname = `${name} bobade`;
+
+console.log(fullname);
+
+console.log(fullname.charAt(0));
+
+console.log(fullname.endsWith("e"));
+
+console.log(fullname.includes("bob"));
+
+console.log(fullname.toUpperCase());
